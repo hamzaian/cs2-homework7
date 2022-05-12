0c2f76e7f3b3a3afa3f4f7687d7e669ac93f6a8f
Hamza <hamzah.sadiq@gmail.com>
2022-05-12 14:29:08 -0700

    fixed tokenize and createMap

--- src/homework7.cpp
+++ src/homework7.cpp
@@ -47,7 +47,16 @@ int main() {
     inputFile.close();
 
     // TODO: iterate over the map to display the pairs
+    map<string, set<unsigned>>::iterator it_print = result.begin();
+    for(it_print; it_print != result.end(); it_print++){
+        cout << "printing key" << it_print->first << " ";//<< " " << it->second <<
+        set<unsigned>::iterator it_set = it_print->second.begin();
+        for(it_set; it_set != it_print->second.end(); it_set++){
+            cout << *it_set << " ";
+        };
+        cout << endl;
 
+    }
     // terminate
     return 0;
 }
@@ -62,10 +71,23 @@ vector<string> tokenize(string input){
     //     }
     // }
     int i = 0;
-    while(i!=string::npos){
+    while(true){
+        cout << "hi\n";
         i = input.find(space);  //maybe convert space back to  " " quotes
+        cout << i << endl;
+        if(i==string::npos){
+            cout << "--" << input <<"--" << endl;
+            cout << "here\n";
+
+            tokens.push_back(input); 
+            cout << "here\n";
+
+            return tokens; 
+        }
+        cout << "here\n";
         tokens.push_back(input.substr(0,i));  //or maybe it should end at i-1 since you don't wanna include spaces
-        input.erase(0,i);
+        cout << "||" << input <<"||" << endl;
+        input.erase(0,i+1);
     }
 
 };
@@ -78,14 +100,30 @@ map<string, set<unsigned>> createMap(ifstream& file){
     while(getline(file, line)){
         tokens = tokenize(line);
         for(string word : tokens){
+            cout << "1\n";
             it = output.find(word);
+                        cout << "2\n";
+
             if(it!= output.end()){
+                            cout << "3\n";
+
                 it->second.insert(line_num);
+                            cout << "4\n";
+
             }
             else{
-                output.insert(pair<string, set<unsigned>>(word, set<unsigned>()));
-                it = output.find(word);
+                            cout << "5\n";
+
+                output.insert(pair<string, set<unsigned> >(word, set<unsigned>()));
+                cerr << "6\n";
+                cerr << "7\n";
+
+                // it = output.find(word);
+                cout << "7\n";
+
                 if(it!= output.end()){
+                    cout << "8\n";
+
                     it->second.insert(line_num);
                 }
                 else{
@@ -93,10 +131,12 @@ map<string, set<unsigned>> createMap(ifstream& file){
                 }
                 // pair.second.insert(line_num);
             }
-        }
+        };
 
         line_num++;
     }
+    cout << "end\n";
+    return output;
 
     //create ordered map??
     //use getline
